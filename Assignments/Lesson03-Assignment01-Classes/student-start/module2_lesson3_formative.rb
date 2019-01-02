b04256f9103eb808e6e00710bd50a11263946e71
Jing Wang <jwang302@gmail.com>
2019-01-02 12:33:32 -0800

    Assignment 3

--- Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -1,33 +1,41 @@
 class Person
-  #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
+  # have a first_name and last_name attribute with public accessors
+  # attr_accessor
 
-  #have a class attribute called `people` that holds an array of objects
+  # have a class attribute called `people` that holds an array of objects
   @@people = []
 
-  #have an `initialize` method to initialize each instance
-  def initialize(x,y)#should take 2 parameters for first_name and last_name
-    #assign those parameters to instance variables
-    #add the created instance (self) to people class variable
+  attr_accessor :last_name, :first_name
+
+  # have an `initialize` method to initialize each instance
+  # should take 2 parameters for first_name and last_name
+  def initialize(first_name, last_name)
+    # assign those parameters to instance variables
+    # add the created instance (self) to people class variable
+    @first_name = first_name
+    @last_name = last_name
+    @@people.push(self)
   end
 
-  #have a `search` method to locate all people with a matching `last_name`
+  # have a `search` method to locate all people with a matching `last_name`
   def self.search(last_name)
-    #accept a `last_name` parameter
-    #search the `people` class attribute for instances with the same `last_name`
-    #return a collection of matching instances
+    # accept a `last_name` parameter
+    # search the `people` class attribute for instances with the same `last_name`
+    # return a collection of matching instances
+    @@people.select { |person| person.last_name == last_name }
   end
 
-  #have a `to_s` method to return a formatted string of the person's name
+  # have a `to_s` method to return a formatted string of the person's name
   def to_s
-    #return a formatted string as `first_name(space)last_name`
+    # return a formatted string as `first_name(space)last_name`
+    "#{@first_name} #{@last_name}"
   end
 end
 
-p1 = Person.new("John", "Smith")
-p2 = Person.new("John", "Doe")
-p3 = Person.new("Jane", "Smith")
-p4 = Person.new("Cool", "Dude")
+Person.new("John", "Smith")
+Person.new("John", "Doe")
+Person.new("Jane", "Smith")
+Person.new("Cool", "Dude")
 
 puts Person.search("Smith")
 
